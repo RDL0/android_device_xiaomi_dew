12dad4dade671827915aac4658f3f7646d95aa58
RDL0 <rahmdrei1@gmail.com>
2026-05-22 21:49:08 +0800

    Fix Inclussions bug

--- twrp_dew.mk
+++ twrp_dew.mk
@@ -1,17 +1,12 @@
-# Inherit from standard build architecture configurations
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_caps.mk)
-
-# Inherit TWRP minimal setup variables
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Unified Device Identity Flags
+# Unified Device Identity
 PRODUCT_DEVICE := dew
 PRODUCT_NAME := twrp_dew
 PRODUCT_BRAND := REDMI
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Display Configuration for the UI Screen
+# Models
 PRODUCT_MODEL := Redmi 15C / Poco C85
 PRODUCT_RELEASE_NAME := Redmi 15C / Poco C85
 
